898586ebc76e29be34b1f20860a3dd7fd79a6db6
IsaíasDZ <danielavilesdiazdiaz@gmail.com>
2024-08-01 06:56:39 -0600

    add images articles

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -7,19 +7,20 @@ import { FaRegMessage } from "react-icons/fa6";
 import { FaRegShareFromSquare } from "react-icons/fa6";
 import { IoArrowRedoSharp } from "react-icons/io5";
 
+const img2 =  "usuario.jpg"
 
 function Articulos() {
   const dataComp = Data.map((data, index)=> {
     return(
       <div className="articulos-div" key={index}>
         <div className="box-title">
-            <p className="nombre-testimonio"><strong>{data.nombre}</strong> {data.pais}</p>
+            <p className="nombre-testimonio"><strong>{data.nombre}</strong></p>
             <p className="nombre-favorito"><IoBookmarks className="book"/></p>
         </div>
         
       <div className="contenedor-testimonio">
         
-            <img className="imagen-testimonio" src={require(`../img/${data.img}`)} alt="foto-de-daniel"/>
+            <img className="imagen-testimonio" src={require(`../img/articulos/${data.img}`)} alt="foto-de-daniel"/>
             <div className="contenedor-texto-testimonio">
                 <p className="texto-testimonio">{data.texto}</p>
             </div>
@@ -32,73 +33,73 @@ function Articulos() {
       </div>
       <div className="body-comments">
         <div className="box-comments">
-          <img className="img-comment" src={require(`../img/${data.img}`)} alt="foto-de-daniel"/>
+          <img className="img-comment" src={require(`../img/${img2}`)} alt="foto-de-daniel"/>
           <input class="form-control form-control-lg" type="text" placeholder="Escribe un comentario..." aria-label="Escribe un comentario..."/>
           <button class="btn btn-primary">Enviar</button>
           
         </div>
         <div className="comentario1">
-        <img className="img-respuesta" src={require(`../img/${data.img2}`)} alt="foto-de-daniel"/>
+        <img className="img-respuesta" src={require(`../img/${img2}`)} alt="foto-de-daniel"/>
           <h1>Usuario</h1>
           <p className="box-comentario">{data.comentario1}</p>
           <p className="botones"><IoArrowRedoSharp />Responder</p>
         </div>
         <div className="comentario1">
-        <img className="img-respuesta" src={require(`../img/${data.img2}`)} alt="foto-de-daniel"/>
+        <img className="img-respuesta" src={require(`../img/${img2}`)} alt="foto-de-daniel"/>
           <h1>Usuario</h1>
           <p className="box-comentario">{data.comentario2}</p>
           <p className="botones"><IoArrowRedoSharp />Responder</p>
         </div>
         <div className="comentario1">
-        <img className="img-respuesta" src={require(`../img/${data.img2}`)} alt="foto-de-daniel"/>
+        <img className="img-respuesta" src={require(`../img/${img2}`)} alt="foto-de-daniel"/>
           <h1>Usuario</h1>
           <p className="box-comentario">{data.comentario3}</p>
           <p className="botones"><IoArrowRedoSharp />Responder</p>
         </div>
         <div className="comentario1">
-        <img className="img-respuesta" src={require(`../img/${data.img2}`)} alt="foto-de-daniel"/>
+        <img className="img-respuesta" src={require(`../img/${img2}`)} alt="foto-de-daniel"/>
           <h1>Usuario</h1>
           <p className="box-comentario">{data.comentario4}</p>
           <p className="botones"><IoArrowRedoSharp />Responder</p>
         </div>
         <div className="comentario1">
-        <img className="img-respuesta" src={require(`../img/${data.img2}`)} alt="foto-de-daniel"/>
+        <img className="img-respuesta" src={require(`../img/${img2}`)} alt="foto-de-daniel"/>
           <h1>Usuario</h1>
           <p className="box-comentario">{data.comentario5}</p>
           <p className="botones"><IoArrowRedoSharp />Responder</p>
         </div>
         <div className="comentario1">
-        <img className="img-respuesta" src={require(`../img/${data.img2}`)} alt="foto-de-daniel"/>
+        <img className="img-respuesta" src={require(`../img/${img2}`)} alt="foto-de-daniel"/>
           <h1>Usuario</h1>
           <p className="box-comentario">{data.comentario6}</p>
           <p className="botones"><IoArrowRedoSharp />Responder</p>
         </div>
         <div className="comentario1">
-        <img className="img-respuesta" src={require(`../img/${data.img2}`)} alt="foto-de-daniel"/>
+        <img className="img-respuesta" src={require(`../img/${img2}`)} alt="foto-de-daniel"/>
           <h1>Usuario</h1>
           <p className="box-comentario">{data.comentario7}</p>
           <p className="botones"><IoArrowRedoSharp />Responder</p>
         </div>
         <div className="comentario1">
-        <img className="img-respuesta" src={require(`../img/${data.img2}`)} alt="foto-de-daniel"/>
+        <img className="img-respuesta" src={require(`../img/${img2}`)} alt="foto-de-daniel"/>
           <h1>Usuario</h1>
           <p className="box-comentario">{data.comentario8}</p>
           <p className="botones"><IoArrowRedoSharp />Responder</p>
         </div>
         <div className="comentario1">
-        <img className="img-respuesta" src={require(`../img/${data.img2}`)} alt="foto-de-daniel"/>
+        <img className="img-respuesta" src={require(`../img/${img2}`)} alt="foto-de-daniel"/>
           <h1>Usuario</h1>
           <p className="box-comentario">{data.comentario9}</p>
           <p className="botones"><IoArrowRedoSharp />Responder</p>
         </div>
         <div className="comentario1">
-        <img className="img-respuesta" src={require(`../img/${data.img2}`)} alt="foto-de-daniel"/>
+        <img className="img-respuesta" src={require(`../img/${img2}`)} alt="foto-de-daniel"/>
           <h1>Usuario</h1>
           <p className="box-comentario">{data.comentario10}</p>
           <p className="botones"><IoArrowRedoSharp />Responder</p>
         </div>
         <div className="comentario1">
-        <img className="img-respuesta" src={require(`../img/${data.img2}`)} alt="foto-de-daniel"/>
+        <img className="img-respuesta" src={require(`../img/${img2}`)} alt="foto-de-daniel"/>
           <h1>Usuario</h1>
           <p className="box-comentario">{data.comentario7}</p>
           <p className="botones"><IoArrowRedoSharp />Responder</p>
